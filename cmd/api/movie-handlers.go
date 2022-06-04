a14478710f4695a26815d8af5e741b4e02e56c60
Breno Santana <brenosantanabruno@live.com>
2022-06-04 18:56:32 -0300

    initial commit

---
+++ cmd/api/movie-handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"backend/models"
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("Invalid ID Parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+	app.logger.Println("id is ", id)
+
+	movie := models.Movie{
+		ID:          id,
+		Title:       "Some movie",
+		Description: "Some description",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+		Runtime:     100,
+		Rating:      5,
+		MPAARating:  "PG-13",
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
